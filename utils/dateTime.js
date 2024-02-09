df7cb7bbb64c862926702ef2dd6f3551d371536a
Swasti Saha Roy <133259637+Swa-sti@users.noreply.github.com>
2024-02-09 19:18:43 +0530

    Update dateTime.js

--- utils/dateTime.js
+++ utils/dateTime.js
@@ -3,7 +3,7 @@ function getCurrentDate() {
       'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
       'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
     ];
-  
+    
     const currentDate = new Date();
     const month = months[currentDate.getMonth()];
     const day = currentDate.getDate();
